5c52e394262ce7b932b39edd24893ee2ba2a9348
darenallan <tienddaren4@gmail.com>
2026-05-12 11:36:01 +0100

    integration firebase

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,8 +1,16 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseConfig.json");
+
+const firebaseConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  // Remplacer les \n échappés par de vrais sauts de ligne pour la clé privée
+  privateKey: process.env.FIREBASE_PRIVATE_KEY 
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') 
+    : undefined,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
 });
 
 const db = admin.firestore();
